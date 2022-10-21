00970705bd5980c8cddd16fd3427e93c6617680e
asif <muhammadasifashraf270@gmail.com>
2022-10-21 15:48:49 +0400

    Day1

--- src/Day1.java
+++ src/Day1.java
@@ -1,13 +1,60 @@
+import java.util.Arrays;
+
 public class Day1 {
 
 public static void  main(String[] args){
-    int[] input={1,2,3,4,5};
-    reverseString("hello");
+    int[] input={1,-2,3};
+    rearrancePositiveNo(input);
 }
 
-public static void rearranceOrder(int[] arr){
-    int[] positiveArray=new int[arr.length];
+public static void rearrancePositiveNo(int[] arr){
 
+    for (int i=1;i<arr.length;i++){
+        int temp=arr[i];
+        if(arr[i]>0)
+            continue;
+        while (i>=0 && arr[i-1]>0){
+            arr[i]=arr[i+1];
+        }
+        arr[arr.length-1]=temp;
+    }
+    for (int i=0;i<arr.length;i++){
+        System.out.println(arr[i]);
+    }
+
+
+}
+public static void moveZeroLeft(int[] arr){
+    for (int i=arr.length-1;i>0;i--){
+        if(arr[i]==0){
+            int temp=arr[i];
+            for (int j=i;j>0;j--){
+                arr[j]=arr[j-1];
+            }
+            arr[0]=temp;
+        }
+    }
+    for (int i=0;i<arr.length;i++){
+        System.out.println(arr[i]);
+    }
+}
+
+
+public static void moveZeroRight(int[] arr){
+    for (int i=0;i<arr.length;i++){
+        if(arr[i]==0){
+            int temp=arr[i];
+            for (int j=i;j<arr.length-1;j++){
+                arr[j]=arr[j+1];
+
+            }
+            arr[arr.length-1]=temp;
+
+        }
+    }
+    for (int i=0;i<arr.length;i++){
+        System.out.println(arr[i]);
+    }
 }
     public static void rotateArry(int[] arr,int d){
         int p=0;
